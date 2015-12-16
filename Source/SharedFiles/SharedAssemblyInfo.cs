f91fa4781b4f3a95b55ace87afab252f66850ba3
johannesvaltonen <johannes.valtonen@affecto.com>
2015-12-16 15:42:49 +0200

    Added build version.

--- Source/SharedFiles/SharedAssemblyInfo.cs
+++ Source/SharedFiles/SharedAssemblyInfo.cs
@@ -4,4 +4,4 @@
 
 [assembly: AssemblyVersion("0.11.0.0")]
 [assembly: AssemblyFileVersion("0.11.0.0")]
-[assembly: AssemblyInformationalVersion("0.11.0")]
+[assembly: AssemblyInformationalVersion("0.11.0.0")]
